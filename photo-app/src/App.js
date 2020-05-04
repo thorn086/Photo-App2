e17b9454dc3acf12c2dd009512f2ae54ce3a0c03
Darren Rose <thorn086@gmail.com>
2020-05-03 21:37:36 -0500

    added react

---
+++ photo-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <p>Test that app Works!</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
